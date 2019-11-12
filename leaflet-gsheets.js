b9cc6baf41e128482fd0f068c7ae2f3f084f918e
Andy McIntyre <andrewbmcintyre@outlook.com>
2019-11-12 11:07:01 +0000

    Changed website heading

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -1,29 +1,28 @@
-
 /*
  * Script to display two tables from Google Sheets as point and polygon layers using Leaflet
  * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
  */
 
-
 // init() is called as soon as the page loads
 function init() {
 
-  // PASTE YOUR URLs HERE
-  // these URLs come from Google Sheets 'shareable link' form
-  // the first is the polygon layer and the second the points
-  var polyURL = 'https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing';
+  // PASTE YOUR URLs HERE from Google Sheets 'shareable link' form
+  // the first WILL BE the asset 1 layer and the second the asset 2 layer
+  //var polyURL = 'https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing';
   var pointsURL = 'https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing';
 
-  Tabletop.init( { key: polyURL,
-    callback: addPolygons,
-    simpleSheet: true } );
+//this is my URL for Google Sheets workbook: 'https://docs.google.com/spreadsheets/d/17EVLKmK7-52xxPOZUPLp1DiOfY33WOZXrSHjGlZgcqI/edit?usp=sharing'
+
+  //Tabletop.init( { key: polyURL,
+    //callback: addPolygons,
+    //simpleSheet: true } );
   Tabletop.init( { key: pointsURL,
     callback: addPoints,
     simpleSheet: true } );  // simpleSheet assumes there is only one table and automatically sends its data
 }
 window.addEventListener('DOMContentLoaded', init);
 
-// Create a new Leaflet map centered on the continental US
+// Create a new Leaflet map centered on the continental US CHANGE!
 var map = L.map('map').setView([40, -100], 4);
 
 // This is the Carto Positron basemap
@@ -54,76 +53,76 @@ map.on('click', function (feature, layer) {
 });
 
 // These are declared outisde the functions so that the functions can check if they already exist
-var polygonLayer;
+//var polygonLayer;
 var pointGroupLayer;
 
 // The form of data must be a JSON representation of a table as returned by Tabletop.js
 // addPolygons first checks if the map layer has already been assigned, and if so, deletes it and makes a fresh one
 // The assumption is that the locally stored JSONs will load before Tabletop.js can pull the external data from Google Sheets
-function addPolygons(data) {
-  if (polygonLayer != null) {
+//function addPolygons(data) {
+  //if (polygonLayer != null) {
     // If the layer exists, remove it and continue to make a new one with data
-    polygonLayer.remove()
-  }
+    //polygonLayer.remove()
+ // }
 
   // Need to convert the Tabletop.js JSON into a GeoJSON
   // Start with an empty GeoJSON of type FeatureCollection
   // All the rows will be inserted into a single GeoJSON
-  var geojsonStates = {
-    'type': 'FeatureCollection',
-    'features': []
-  };
+  //var geojsonStates = {
+  //  'type': 'FeatureCollection',
+   // 'features': []
+  //};
 
-  for (var row in data) {
+  //for (var row in data) {
     // The Sheets data has a column 'include' that specifies if that row should be mapped
-    if (data[row].include == 'y') {
-      var coords = JSON.parse(data[row].geometry);
+//     if (data[row].include == 'y') {
+//       var coords = JSON.parse(data[row].geometry);
 
-      geojsonStates.features.push({
-        'type': 'Feature',
-        'geometry': {
-          'type': 'MultiPolygon',
-          'coordinates': coords
-        },
-        'properties': {
-          'name': data[row].name,
-          'summary': data[row].summary,
-          'state': data[row].state,
-          'local': data[row].local,
-        }
-      });
-    }
-  }
+//       geojsonStates.features.push({
+//         'type': 'Feature',
+//         'geometry': {
+//           'type': 'MultiPolygon',
+//           'coordinates': coords
+//         },
+//         'properties': {
+//           'name': data[row].name,
+//           'summary': data[row].summary,
+//           'state': data[row].state,
+//           'local': data[row].local,
+//         }
+//       });
+//     }
+//   }
 
-  // The polygons are styled slightly differently on mouse hovers
-  var polygonStyle = {'color': '#2ca25f', 'fillColor': '#99d8c9', 'weight': 1.5};
-  var polygonHoverStyle = {'color': 'green', 'fillColor': '#2ca25f', 'weight': 3};
+//   // The polygons are styled slightly differently on mouse hovers
+//   var polygonStyle = {'color': '#2ca25f', 'fillColor': '#99d8c9', 'weight': 1.5};
+//   var polygonHoverStyle = {'color': 'green', 'fillColor': '#2ca25f', 'weight': 3};
 
-  polygonLayer = L.geoJSON(geojsonStates, {
-    onEachFeature: function (feature, layer) {
-      layer.on({
-        mouseout: function(e) {
-          e.target.setStyle(polygonStyle);
-        },
-        mouseover: function(e) {
-          e.target.setStyle(polygonHoverStyle);
-        },
-        click: function(e) {
-          // This zooms the map to the clicked polygon
-          // map.fitBounds(e.target.getBounds());
+//   polygonLayer = L.geoJSON(geojsonStates, {
+//     onEachFeature: function (feature, layer) {
+//       layer.on({
+//         mouseout: function(e) {
+//           e.target.setStyle(polygonStyle);
+//         },
+//         mouseover: function(e) {
+//           e.target.setStyle(polygonHoverStyle);
+//         },
+//         click: function(e) {
+//           // This zooms the map to the clicked polygon
+//           // map.fitBounds(e.target.getBounds());
 
-          // if this isn't added, then map.click is also fired!
-          L.DomEvent.stopPropagation(e);
+//           // if this isn't added, then map.click is also fired!
+//           L.DomEvent.stopPropagation(e);
 
-          document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.name;
-          document.getElementById('sidebar-content').innerHTML = e.target.feature.properties.summary;
-          sidebar.open(panelID);
-        }
-      });
-    },
-    style: polygonStyle
-  }).addTo(map);
-}
+//           document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.name;
+//           document.getElementById('sidebar-content').innerHTML = e.target.feature.properties.summary;
+//           sidebar.open(panelID);
+//         }
+//       });
+//     },
+//     style: polygonStyle
+//   }).addTo(map);
+// }
 
 // addPoints is a bit simpler, as no GeoJSON is needed for the points
 // It does the same check to overwrite the existing points layer once the Google Sheets data comes along
